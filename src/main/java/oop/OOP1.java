4e810b84b14887c74f162613519ce7a56bb0219a
user <mahury@ya.ru>
2021-11-27 20:40:17 +0300

    meaw

--- src/main/java/oop/OOP1.java
+++ src/main/java/oop/OOP1.java
@@ -6,6 +6,38 @@ public class OOP1 {
         System.out.println(p.name);
         System.out.println(p.city);
         System.out.println(p.age);
+        Person[] p_arr = new Person[3];
+        p_arr[0] = new Person("jack", "Moscow", 32);
+        p_arr[1] = new Person("Mike", "Moscow", 34);
+        p_arr[2] = new Person("nike", "Moscow", 33);
+        for (Person person : p_arr) {
+            System.out.println(person.name + " " + person.city + " " + person.age);
+        }
+        for (Person temp : p_arr){
+            System.out.println(temp.name + " " + temp.city + " "+ temp.age);
+
+        }
+        User[] u_arr = new User[3];
+        u_arr[0]= new User(1,"one","123");
+        u_arr[1]= new User(2,"two","1235");
+        u_arr[2]= new User(3,"three","1234");
+
+        for (User user : u_arr) {
+            System.out.println("id=" + user.id + "; логин=" + user.login + "; password=" + user.password);
+        }
+        Person p1 = new Person("Igor","omsk",20);
+        System.out.println(p1.getTextInfo());
+
+        p1.setName("oleg");
+        p1.setCity("orsk");
+        p1.setAge(76);
+        System.out.println(p1.getAge());
+        System.out.println(p1.getCity());
+        System.out.println(p1.getName());
+        Person p2 = new Person("nik", "tver", 82);
+        p2.whoIsOlder(p1);
+
+
 
     }
 
--- src/main/java/oop/Person.java
+++ src/main/java/oop/Person.java
@@ -10,4 +10,37 @@ public class Person {
         this.city=city;
         this.age=age;
     }
+    public String getTextInfo (){
+        return "name " + this.name + " city "+ this.city + " age " + this.age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void whoIsOlder(Person p){
+        if (this.age > p.age) System.out.println(this.name + " older than " + p.name);
+        else if (this.age== p.age) System.out.println(" совпадают ");
+        else System.out.println( "младше");
+    }
 }
