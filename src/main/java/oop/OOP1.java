01e6fce402b51054194d6c6a76b97994555ffb22
user <mahury@ya.ru>
2021-11-27 18:18:27 +0300

    meaw

--- src/main/java/oop/OOP1.java
+++ src/main/java/oop/OOP1.java
@@ -2,6 +2,10 @@ package oop;
 
 public class OOP1 {
     public static void main(String[] args){
+        Person p = new Person("Michael", "Moscow", 37);
+        System.out.println(p.name);
+        System.out.println(p.city);
+        System.out.println(p.age);
 
     }
 
---
+++ src/main/java/oop/Person.java
@@ -0,0 +1,13 @@
+package oop;
+
+public class Person {
+    String name;
+    String city;
+    int age;
+
+    public Person(String name, String city, int age){
+        this.name=name;
+        this.city=city;
+        this.age=age;
+    }
+}
